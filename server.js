705c8222592d736cffb0868483adc29e4c3caf65
hye-yeong lee <i-e@yukoyuko.co.jp>
2023-07-28 16:57:48 +0900

    feat: add new datas, resolver

--- server.js
+++ server.js
@@ -3,31 +3,53 @@ import {
   gql
 } from "apollo-server"
 
+const tweets = [{
+  id: "1",
+  text: "first",
+}, {
+  id: "2",
+  text: "second",
+}]
+
 // graphQl SDL (schema definition language)
 // Query って言うタイプを書くべき
 //Mutationは、データを修正；削除’生成をするために使う
 const typeDefs = gql `
   type User {
-    id: ID
-    username: String 
+    id: ID!
+    username: String !
   }
   type Tweet {
     id: ID
-    text: String
+    text: String!
     author: User
   }
   type Query { 
     allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    tweet(id: ID!): Tweet
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `
 
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets
+    },
+    tweet(root, {
+      id
+    }) {
+      return tweets.find((tweet) => tweet.id === id)
+    },
+  }
+}
+
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  resolvers
 })
 server.listen().then(({
   url
